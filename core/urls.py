581932119832b0dd04c180d43eb75295c40a25c2
Aldo Cesar <57098697+Aldo-lima@users.noreply.github.com>
2021-05-12 09:00:39 -0300

    crude entradas

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,20 @@
 from django.urls import path, include
 from .views import (home, lista_pessoas, lista_veiculos, lista_movMensalista,
-                    lista_movRotativo, lista_mensalista)
+                    lista_movRotativo, lista_mensalista, pessoa_nova, veiculo_novo,
+                    mensalista_novo, movMensalista_novo, movRotativo_novo, pessoa_update)
 
 urlpatterns = [
 
     path('', home, name='core_home'),
     path('pessoa', lista_pessoas, name='core_lista_pessoa'),
-    path('veiculo', lista_veiculos, name='lista_veiculos'),
+    path('update_pessoa/<id>', pessoa_update, name='pessoa_update'),
+    path('pessoa-nova', pessoa_nova, name='core_pesoa_nova'),
+    path('veiculo', lista_veiculos, name='core_lista_veiculos'),
+    path('veiculo-novo', veiculo_novo, name='core_veiculo_novo'),
     path('movMensalista', lista_movMensalista, name='lista_movMensalista'),
-    path('movRotativo', lista_movRotativo, name='lista_movRotativo'),
-    path('mensalista', lista_mensalista, name='lista_mensalista'),
+    path('movMensalista-novo', movMensalista_novo, name='core_movMensalista_novo'),
+    path('movRotativo', lista_movRotativo, name='core_lista_movRotativo'),
+    path('movRotativo-novo', movRotativo_novo, name='core_movRotativo_novo'),
+    path('mensalista', lista_mensalista, name='core_lista_mensalista'),
+    path('mensalista-novo', mensalista_novo, name='core_mensalista_novo'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,33 +1,95 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+#from django.http import HttpResponse
 from .models import Pessoa, Veiculo, MovMensalista, ModelRotativo, Mensalista
+from .form import (PessoaForm, VeiculoForm, MensalistaForm,
+                   MovRotativoForm,  MovMensalistaForm)
 # Create your views here.
 
 def home(request):
     context = {'mensagem': 'olá mundo'}
     return render(request, 'core/index.html', context)
 
+#========================Pessoas==========================
 
 def lista_pessoas(request):
+    form = PessoaForm()
     pessoas = Pessoa.objects.all()
-    return render(request, 'core/lista_pessoas.html', {'pessoas':pessoas})
+    data = {'pessoas': pessoas, 'form':form}
+    return render(request, 'core/lista_pessoas.html', data)
+
+
+def pessoa_nova(request, id):
+    form = PessoaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('core_lista_pessoa')
+
+
+def pessoa_update(request, id):
+        data = {}
+        pessoa = Pessoa.objects.get(id=id)
+        form = PessoaForm(request.POST or None, instance=pessoa)
+        data['pessoa'] = pessoa
+        data['form'] = form
+
+        if request.method == 'POST':
+            if form.is_valid():
+                form.save()
+                return redirect('core_lista_pessoa')
+        else:
+            return render(request, 'core/up_date_pessoa.html', data)
+
+#====================================Veiculo=======================
 
 def lista_veiculos(request):
+    form = VeiculoForm()
     veiculos = Veiculo.objects.all()
-    return render(request, 'core/lista_veiculo.html', {'veiculos':veiculos})
+    data = {'veiculos':veiculos, 'form': form}
+    return render(request, 'core/lista_veiculo.html', data)
+
+
+def veiculo_novo(request):
+    form = VeiculoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('core_lista_veiculos')
+
+
+#==================================Movimento Mensalista
+
 
 def lista_movMensalista(request):
+    form = MovMensalistaForm()
     movMensalistas = MovMensalista.objects.all()
-    return render(request, 'core/lista_MovMensalistas.html', {'movMensalistas':movMensalistas})
+    data = {'movMensalistas': movMensalistas, 'form': form}
+    return render(request, 'core/lista_MovMensalistas.html', data)
+
+def movMensalista_novo(request):
+    form = MovMensalistaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('lista_movMensalista')
 
 def lista_movRotativo(request):
+    form =MovRotativoForm()
     movRotativos = ModelRotativo.objects.all()
-    return render(request, 'core/lista_MovRotativo.html', {'movRotativos':movRotativos})
+    data = {'movRotativos':movRotativos, 'form': form}
+    return render(request, 'core/lista_MovRotativo.html', data)
 
-def lista_movRotativo(request):
-    movRotativos = ModelRotativo.objects.all()
-    return render(request, 'core/lista_MovRotativo.html', {'movRotativos':movRotativos})
+def movRotativo_novo(request):
+    form = MovRotativoForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('core_lista_movRotativo')
 
 def lista_mensalista(request):
+     form = MensalistaForm()
      mensalistas = Mensalista.objects.all()
-     return render(request, 'core/lista_mensalista.html', {'mensalistas':mensalistas})
+     data = {'mensalistas': mensalistas, 'form': form}
+     return render(request, 'core/lista_mensalista.html', data)
+
+def mensalista_novo(request):
+    form = MensalistaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+    return redirect('core_lista_mensalista')
